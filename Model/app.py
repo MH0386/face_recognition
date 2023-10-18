a6c44e8e9ec7b9e75f314d08f48f0681963b0c3e
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2023-10-18 19:16:42 +0300

    init

--- Model/app.py
+++ Model/app.py
@@ -6,12 +6,14 @@ face_classifier = cv.CascadeClassifier(
 
 video_capture = cv.VideoCapture(0)
 
+
 def detect_bounding_box(vid):
     gray_image = cv.cvtColor(vid, cv.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
     for (x, y, w, h) in faces:
         cv.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 5)
     return faces
+
 
 while True:
 
